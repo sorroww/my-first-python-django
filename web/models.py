baa4926d2fa4edc67ae39e3aaeedb9bdffc17a9c
sorrow <ali.shahvar.it@gmail.com>
2018-09-14 16:12:56 +0430

    default

--- web/models.py
+++ web/models.py
@@ -9,8 +9,12 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return 'in this date my expense is ::::{}---{}'.format(self.date, self.amount)
 class Income(models.Model):
     text = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
     date = models.DateTimeField()
+    def __unicode__(self):
+        return 'at this moment my income is :: {}---{}'.format(self.date, self.amount)    
