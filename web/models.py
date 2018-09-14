42e0d1b0b0a8b367835c193a7156e0db609cc1d5
sorrow <ali.shahvar.it@gmail.com>
2018-09-14 14:51:34 +0430

    create Expense Successfully

--- web/models.py
+++ web/models.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User)
